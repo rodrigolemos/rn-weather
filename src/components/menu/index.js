e076a2fcf39df0361672821275e9ff5284620ec4
Rodrigo <rodrigolemosrl@hotmail.com>
2021-03-16 20:16:21 -0300

    Change stylesheet to styled-components

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -1,37 +1,20 @@
 import React from 'react';
-import { StyleSheet, TouchableOpacity } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
+
+import { Container } from './styles';
 
 export default function Menu() {
   const navigation = useNavigation();
 
   return (
-    <TouchableOpacity
-      style={styles.container}
+    <Container
       onPress={() => navigation.openDrawer()}>
       <Feather
         name="menu"
         size={28}
         color="#373737"
       />
-    </TouchableOpacity>
+    </Container>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    position: 'absolute',
-    zIndex: 9,
-    width: 60,
-    height: 60,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    left: 25,
-    top: 25,
-    borderTopRightRadius: 25,
-    borderBottomRightRadius: 25,
-    borderBottomLeftRadius: 25
-  }
-})
---
+++ src/components/menu/styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.TouchableOpacity`
+  position: absolute;
+  z-index: 9;
+  left: 25;
+  top: 25;
+  width: 60px;
+  height: 60px;
+  background-color: #fff;
+  align-items: center;
+  justify-content: center;
+  border-top-right-radius: 25px;
+  border-top-left-radius: 25px;
+  border-bottom-left-radius: 25px;
+`;
